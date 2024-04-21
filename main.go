dc75dfd83406102d32f13914045aa9964086398a
Dharmendra Kashaudhan <kashaudhan94@gmail.com>
2024-04-22 01:53:00 +0530

    chore: set & get event

--- main.go
+++ main.go
@@ -4,16 +4,55 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"rest.api/db"
+	"rest.api/models"
 )
 
 func main() {
+	db.InitDB()
+	
 	server := gin.Default()
 
-	server.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H {
-			"message": "Running",
-		})
-	})
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
+}
+
+func getEvents(ctx *gin.Context) {
+
+	events, err := models.GetAllEvents()
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Something went wrong",
+		})
+	}
+
+	ctx.JSON(http.StatusOK, events)
+}
+
+func createEvent(ctx *gin.Context) {
+	var event models.Event
+	err := ctx.ShouldBindJSON(&event)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid data provided",
+		})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	err = event.Save()
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Failed to save data",
+		})
+	}
+
+	ctx.JSON(http.StatusCreated, event)
 }
